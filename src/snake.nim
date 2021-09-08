bbb281e91aa2de8d117e80d3034294bd88041306
Milo Weinberg <iapetus011@gmail.com>
2021-09-08 16:58:42 -0400

    progress

--- src/snake.nim
+++ src/snake.nim
@@ -1,26 +1,42 @@
 import csfml
 
-type
-    Point = tuple[x: int, y: int]
-
 const
-    BACKGROUND_COLOR = color(30, 30, 40)
     WINDOW_X: cint = 800
     WINDOW_Y: cint = 600
     BOARD_X = 75
     BOARD_Y = 50
+    BACKGROUND_COLOR = color(30, 30, 40)
+    SNAKE_HEAD_COLOR = color(30, 225, 75)
+    SNAKE_BODY_COLOR = color(20, 200, 50)
+
+type
+    Point = tuple[x: int, y: int]
+    Snake = seq[Point]
+    Board = array[BOARD_X, array[BOARD_Y, int]]
+
+iterator enumerate[T](s: seq[T]): tuple[i: int, v: T] =
+    var i = 0
+
+    for v in s:
+        yield (i, v)
+        i += 1
+
+proc drawSnake(w: RenderWindow, s: Snake) =
+    var vertices = newVertexArray(PrimitiveType.Quads, s.len)
+    
+    for i, p in enumerate(s):
+        vertices[i] = newRectangleShape(vec2(0, 0))
 
 let
     ctxSettings = ContextSettings(antialiasingLevel: 16)
     window = newRenderWindow(videoMode(WINDOW_X, WINDOW_Y), "Snake", settings = ctxSettings)
-    
 
 window.verticalSyncEnabled = true
 
 var
     event: Event
-    board: array[BOARD_X, array[BOARD_Y, int]]
-    snake: seq[Point]
+    board: Board
+    snake: Snake
 
 while window.open:
     if window.pollEvent(event):
